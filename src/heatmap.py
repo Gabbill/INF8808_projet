4f89bf2c62cee63218363ca8e3123b17f148f3f2
Lana Pham <tam-lan.pham@polymtl.ca>
2024-03-25 16:48:56 -0400

    LP - EW : Qualité du code de heatmap

--- src/heatmap.py
+++ src/heatmap.py
@@ -1,54 +1,51 @@
 # Inspired by : https://github.com/brunorosilva/plotly-calplot/
 
-import pandas as pd
 import numpy as np
 import preprocess
 import plotly.graph_objects as go
 
 from plotly.subplots import make_subplots
 
+YEARS = [2019, 2020, 2021, 2022, 2023]
+MONTH_NAMES = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
+               'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+WEEK_DAYS_NAMES = ['Lundi', 'Mardi', 'Mercredi',
+                   'Jeudi', 'Vendredi', 'Samedi', 'Dimanche']
+MONTH_POSITIONS = np.linspace(1.5, 50, 12)
+
 
 def heatmap(df):
-    years = [2019, 2020, 2021, 2022, 2023]
-    years_as_strings = [str(year) for year in years]
-    nb_years = len(years)
-    nb_columns = 1
-    min_nb_bike_rides = df['nb_passages'].min()
-    max_nb_bike_rides = df['nb_passages'].max()
-
-    fig_height = 150 * nb_years
+    years_as_strings = [str(year) for year in YEARS]
+    nb_years = len(YEARS)
 
     fig = make_subplots(
         rows=nb_years,
-        cols=nb_columns,
+        cols=1,
         subplot_titles=years_as_strings,
         vertical_spacing=0.08,
     )
 
-    for i, year in enumerate(years):
+    for year_index, year in enumerate(YEARS):
         year_df = df.loc[df['date'].dt.year == year]
-        # print(year_df)
-        # fill_empty_with_zeros si ça marche pas
-        yearly_heatmap(year_df, fig, fig_height, i)
+        year_heatmap(year_df, fig, year_index)
 
-    fig = apply_general_colorscaling(
-        fig, min_nb_bike_rides, max_nb_bike_rides)
-    fig = showscale_of_heatmaps(fig)
+    add_color_scale(fig, df['nb_passages'].min(), df['nb_passages'].max())
+
+    fig_height = 150 * nb_years
+    update_layout(fig, fig_height)
 
     return fig
 
 
-def yearly_heatmap(yearly_df, fig, fig_height, row):
-    month_names = ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin',
-                   'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre']
+def year_heatmap(year_df, fig, year_index):
+    week_days = [date.weekday() for date in year_df['date']]
+    week_numbers = year_df['date'].dt.strftime('%W').astype(int).tolist()
 
-    month_positions, week_days, week_numbers = get_date_coordinates(yearly_df)
-
-    yearly_heatmap = [
+    year_heatmap = [
         go.Heatmap(
             x=week_numbers,
             y=week_days,
-            z=yearly_df['nb_passages'],
+            z=year_df['nb_passages'],
             xgap=1,
             ygap=1,
             showscale=False,
@@ -56,101 +53,80 @@ def yearly_heatmap(yearly_df, fig, fig_height, row):
         )
     ]
 
+    add_month_separators(year_heatmap, year_df, week_days, week_numbers)
+    add_year_heatmap(fig, year_heatmap, year_index)
+
+    return fig
+
+
+def add_month_separators(year_heatmap, year_df, week_days, week_numbers):
     month_lines = dict(
         mode='lines',
         line=dict(color='#000000', width=2),
         hoverinfo='skip',
     )
 
-    for date, dow, wkn in zip(yearly_df['date'], week_days, week_numbers):
-        if date.day == 1:
-            yearly_heatmap += [go.Scatter(x=[wkn - 0.5, wkn - 0.5],
-                                          y=[dow - 0.5, 6.5], **month_lines)]
-            if dow:
-                yearly_heatmap += [
-                    go.Scatter(
-                        x=[wkn - 0.5, wkn + 0.5], y=[dow - 0.5, dow - 0.5], **month_lines
-                    ),
-                    go.Scatter(x=[wkn + 0.5, wkn + 0.5],
-                               y=[dow - 0.5, -0.5], **month_lines),
-                ]
+    for date, dow, wkn in zip(year_df['date'], week_days, week_numbers):
+        if date.day != 1:
+            continue
 
-    layout = get_yearly_layout(month_names, month_positions)
-    fig = update_plot_with_layout(fig, yearly_heatmap, layout, fig_height, row)
-
-    return fig
+        year_heatmap += [go.Scatter(x=[wkn - 0.5, wkn - 0.5],
+                                    y=[dow - 0.5, 6.5], **month_lines)]
+        if dow:
+            year_heatmap += [
+                go.Scatter(
+                    x=[wkn - 0.5, wkn + 0.5], y=[dow - 0.5, dow - 0.5], **month_lines
+                ),
+                go.Scatter(x=[wkn + 0.5, wkn + 0.5],
+                           y=[dow - 0.5, -0.5], **month_lines),
+            ]
 
 
-def get_date_coordinates(df):
-    # nb_days_each_month = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
-
-    # if df.loc[df['date'].dt.month == 2, 'date'].max().day == 29:
-    #     nb_days_each_month[1] = 29
-    month_positions = np.linspace(1.5, 50, 12)
-    week_days = [i.weekday() for i in df['date']]
-    week_numbers = df['date'].dt.strftime('%W').astype(int).tolist()
-
-    return month_positions, week_days, week_numbers
+def add_year_heatmap(fig, year_heatmap, year_index):
+    fig.add_traces(
+        year_heatmap,
+        rows=[(year_index + 1)] * len(year_heatmap),
+        cols=[1] * len(year_heatmap)
+    )
 
 
-def get_yearly_layout(month_names, month_positions):
-    return go.Layout(
+def add_color_scale(fig, min_value, max_value):
+    fig.update_traces(
+        zmin=min_value,
+        zmax=max_value,
+        selector=dict(type='heatmap'),
+        showscale=True,
+    )
+
+
+def update_layout(fig, fig_height):
+    layout = go.Layout(
         xaxis=dict(
             showline=False,
             showgrid=False,
             zeroline=False,
             tickmode='array',
-            ticktext=month_names,
-            tickvals=month_positions,
+            ticktext=MONTH_NAMES,
+            tickvals=MONTH_POSITIONS,
+            fixedrange=True,
         ),
         yaxis=dict(
             showline=False,
             showgrid=False,
             zeroline=False,
             tickmode='array',
-            ticktext=['Lundi', 'Mardi', 'Mercredi',
-                      'Jeudi', 'Vendredi', 'Samedi', 'Dimanche'],
+            ticktext=WEEK_DAYS_NAMES,
             tickvals=[0, 1, 2, 3, 4, 5, 6],
             autorange='reversed',
+            fixedrange=True,
         ),
         font=dict(size=10, color='#9e9e9e'),
         plot_bgcolor=('#fff'),
         margin=dict(t=20, b=20),
         showlegend=False,
+        height=fig_height
     )
 
-
-def update_plot_with_layout(fig, yearly_heatmap, layout, fig_height, row):
     fig.update_layout(layout)
-    fig.update_xaxes(layout["xaxis"])
-    fig.update_yaxes(layout["yaxis"])
-    fig.update_layout(height=fig_height)
-
-    rows = [(row + 1)] * len(yearly_heatmap)
-    cols = [1] * len(yearly_heatmap)
-
-    fig.add_traces(yearly_heatmap, rows=rows, cols=cols)
-    return fig
-
-
-def apply_general_colorscaling(fig, min_value, max_value):
-    return fig.update_traces(
-        selector=dict(type='heatmap'), zmax=max_value, zmin=min_value
-    )
-
-
-def showscale_of_heatmaps(fig):
-    return fig.update_traces(
-        showscale=True,
-        selector=dict(type='heatmap'),
-    )
-
-
-# TODO :  À ENLEVER
-bike_counts_data_list = preprocess.load_bike_counts_data_list()
-bike_counts_df = preprocess.get_bike_counts_df(bike_counts_data_list)
-
-daily_bike_count = preprocess.get_daily_bike_count(bike_counts_df)
-
-fig = heatmap(daily_bike_count)
-fig.show()
+    fig.update_xaxes(layout['xaxis'])
+    fig.update_yaxes(layout['yaxis'])
