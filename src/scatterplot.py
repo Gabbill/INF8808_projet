8075bfe43ff3119484b367421722fe164d9339c6
Lana Pham <tam-lan.pham@polymtl.ca>
2024-04-01 11:12:27 -0400

    mini fix qualité du code et grid lines

--- src/scatterplot.py
+++ src/scatterplot.py
@@ -2,34 +2,8 @@ import plotly.graph_objects as go
 import plotly.express as px
 import numpy as np
 import pandas as pd
+import utils
 import hover_template
-
-
-'''
-La fonction suivante va permettre de traduire les mois de la variable 'date' en français 
-pour une meilleur lecture et lisibilité
-'''
-
-
-def translate_date(date_string):
-    MONTH_NAMES = {
-        'January': 'janvier',
-        'February': 'février',
-        'March': 'mars',
-        'April': 'avril',
-        'May': 'mai',
-        'June': 'juin',
-        'July': 'juillet',
-        'August': 'août',
-        'September': 'septembre',
-        'October': 'octobre',
-        'November': 'novembre',
-        'December': 'décembre'
-    }
-    for eng_month, fr_month in MONTH_NAMES.items():
-        if eng_month in date_string:
-            return date_string.replace(eng_month, fr_month)
-    return date_string
 
 
 '''
@@ -42,40 +16,38 @@ moyenne des passages quand la quantité des précipitations est égale à 0.
 
 
 def add_mean_trace(data, x_column):
-    if x_column in ["Total Rain (mm)", "Total Snow (cm)"]:
+    if x_column not in ["Total Rain (mm)", "Total Snow (cm)"]:
+        return
 
-        # Traitement des données et normalisation des valeurs de précipitations égales à 0 par leurs moyenne pour une meilleur lisibilité
-        mean_nb_passages_zero = data.loc[data[x_column]
-                                         == 0, 'nb_passages'].mean()
-        data.loc[data[x_column] == 0, 'nb_passages'] = mean_nb_passages_zero
-        x_values = np.linspace(data[x_column].min(), data[x_column].max(), 100)
-        y_values = np.full_like(x_values, mean_nb_passages_zero)
+    # Traitement des données et normalisation des valeurs de précipitations égales à 0 par leurs moyenne pour une meilleur lisibilité
+    mean_nb_passages_zero = data.loc[data[x_column]
+                                     == 0, 'nb_passages'].mean()
+    data.loc[data[x_column] == 0, 'nb_passages'] = mean_nb_passages_zero
+    x_values = np.linspace(data[x_column].min(), data[x_column].max(), 100)
+    y_values = np.full_like(x_values, mean_nb_passages_zero)
 
-        # On trace notre droite de points horizontale
-        mean_trace = go.Scatter(x=x_values,
-                                y=y_values,
-                                mode='lines',
-                                line=dict(color='#d3d3d3', dash='dash'),
-                                showlegend=True,  # Mettre true pour obtenir la droite comme une legend
-                                name="",
-                                )
+    # On met à jour les informations de la droite de moyenne
+    x_info, x_unit = (
+        "Pluie", "mm") if x_column == "Total Rain (mm)" else ("Neige", "cm")
 
-        # On met à jour les informations de la droite de moyenne
-        x_info, x_unit = (
-            "Pluie", "mm") if x_column == "Total Rain (mm)" else ("Neige", "cm")
-        mean_hover = f"<b style='background-color: #d3d3d3;'> Moyenne de Passages :</b><span> {mean_nb_passages_zero:.0f} </span><br>"
-        mean_hover += f"<b style='background-color: #d3d3d3;'> Quantité de {x_info}:</b><span> 0</span> <span>{x_unit}</span>"
-        mean_trace.update(hovertemplate=mean_hover,
-                          hoverlabel=dict(bgcolor="#EDEDED", font=dict(color="#7E7E7E")))
+    # On trace notre droite de points horizontale
+    mean_trace = go.Scatter(x=x_values,
+                            y=y_values,
+                            mode='lines',
+                            line=dict(color='#9E9E9E', dash='dash'),
+                            showlegend=True,  # Mettre true pour obtenir la droite comme une légende
+                            name=f"Moyenne des passages à 0 {x_unit}",
+                            )
 
-        return mean_trace
-    else:
-        None
+    mean_trace.update(hovertemplate=hover_template.get_mean_scatter_hover_template(mean_nb_passages_zero, x_info, x_unit),
+                      hoverlabel=dict(bgcolor="#EDEDED", font=dict(color="#7E7E7E")))
+
+    return mean_trace
 
 
 '''
 
-La fonction suivante permet de tracer les diffèrents nuages de points,
+La fonction suivante permet de tracer les différents nuages de points.
 On pourra l'utiliser ensuite directement pour choisir la variable à représenter.
 
 '''
@@ -87,8 +59,8 @@ def get_scatterplot_figure(data, x_column, x_title, hover_template):
 
     # Convertir la variable date en structure plus lisible
     data['date'] = pd.to_datetime(data['date'])
-    data['date'] = data['date'].dt.strftime('%d %B %Y')
-    data['date'] = data['date'].apply(translate_date)
+    data['date'] = data['date'].dt.strftime(
+        '%d %B %Y').apply(utils.translate_date)
 
     # Nuage de Points
     fig = px.scatter(data, x=x_column, y='nb_passages',
@@ -98,14 +70,16 @@ def get_scatterplot_figure(data, x_column, x_title, hover_template):
         xaxis_tickangle=0,
         yaxis_title='Nombre de passages (milliers)',
         font=dict(size=12, color='black', family='Roboto'),
-        xaxis=dict(showgrid=True),  # Afficher la grille sur l'axe des x
-        yaxis=dict(showgrid=True),  # Afficher la grille sur l'axe des y
+        xaxis=dict(showgrid=True, gridcolor='#E6E6E6'),
+        yaxis=dict(showgrid=True, gridcolor='#E6E6E6'),
         plot_bgcolor='white',
+        hoverlabel=dict(bgcolor='white')
     )
     fig.update_traces(marker_color='#1f77b4', hovertemplate=hover_template)
 
     # Ajoute la trace de la moyenne si elle a été définie
-    fig.add_trace(mean_trace) if mean_trace else None
+    if mean_trace:
+        fig.add_trace(mean_trace)
 
     return fig
 
@@ -125,17 +99,15 @@ def get_temperature_figure(data):
         'Température', '°C')
     return get_scatterplot_figure(data, 'Mean Temp (°C)', 'Température moyenne (°C)', hover_template_temperature)
 
+
 # Visualisation de la neige
-
-
 def get_snow_figure(data):
     hover_template_neige = hover_template.get_scatter_hover_template(
         'Quantité de neige', 'cm')
     return get_scatterplot_figure(data, 'Total Snow (cm)', 'Quantité de neige (cm)', hover_template_neige)
 
+
 # Visualisation de la pluie
-
-
 def get_rain_figure(data):
     hover_template_pluie = hover_template.get_scatter_hover_template(
         'Quantité de pluie ', 'mm')
