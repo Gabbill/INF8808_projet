fb08894a47134cf4c46b630e509978d4b792bca9
Lana Pham <tam-lan.pham@polymtl.ca>
2024-04-01 11:04:14 -0400

    ajout hover template de moyenne des passages

--- src/hover_template.py
+++ src/hover_template.py
@@ -1,6 +1,6 @@
 
 '''
-La fonction suivante fait répérence à l'info-bulle de la HeatMap.
+La fonction suivante fait référence à l'info-bulle de la HeatMap.
 '''
 
 
@@ -9,7 +9,7 @@ def heatmap_hover_template(week_day, date, bike_count):
 
 
 '''
-La fonction suivante fait répérence à l'info-bulle des nuages de points.
+La fonction suivante fait référence à l'info-bulle des nuages de points.
 '''
 
 
@@ -19,3 +19,14 @@ def get_scatter_hover_template(label, x_unit):
     hover_label += f"<b style='color: #1f77b4;'> {label} :</b>" + \
         "<span > %{customdata[0]}</span>" + f"</span> {x_unit}</span><br>"
     return hover_label
+
+
+'''
+La fonction suivante fait référence à l'info-bulle des nuages de points.
+'''
+
+
+def get_mean_scatter_hover_template(mean_nb_passages_zero, x_info, x_unit):
+    mean_hover = f"<b style='background-color: #d3d3d3;'> Moyenne de Passages :</b><span> {mean_nb_passages_zero:.0f} </span><br>"
+    mean_hover += f"<b style='background-color: #d3d3d3;'> Quantité de {x_info}:</b><span> 0</span> <span>{x_unit}</span>"
+    return mean_hover
