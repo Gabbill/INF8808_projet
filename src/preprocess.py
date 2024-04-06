e5b9fed30a124cc14eb06b79d0dd9a59e5617be9
Nargisse-Benbiga <nargisse.benbiga@enim.ac.ma>
2024-04-06 12:10:21 -0400

    Commentaire preprocess

--- src/preprocess.py
+++ src/preprocess.py
@@ -3,24 +3,43 @@ import geopandas as gpd
 import utils
 
 
+'''
+
+Les fonctions suivantes permettent de charger les diffèrentes bases de données utilisés dans notre projet.
+
+'''
+
+# Chargement des données de comptage de vélos
+
+
 def load_bike_counts_data_list():
     return [pd.read_csv(f'assets/data/comptage_velo_{year}.csv') for year in range(2019, 2025)]
+
+# Chargement des données météorologiques
 
 
 def load_weather_data_list():
     return [pd.read_csv(f'assets/data/en_climate_daily_QC_7025251_{year}_P1D.csv') for year in range(2019, 2025)]
 
+# Chargement des données d'emplacements des compteurs de vélos
+
 
 def load_counters_locations():
     return pd.read_csv('assets/data/localisation_des_compteurs_velo.csv')
+
+# Chargement du fichier comportant le chemin cyclable des vélos
 
 
 def load_montreal_bike_paths():
     return gpd.read_file('assets/data/reseau_cyclable.geojson')
 
+# Combinaison des diffèrentes données de comptages de vélos
+
 
 def get_bike_counts_df(bike_counts_data_list):
     return pd.concat(bike_counts_data_list, ignore_index=True)
+
+# Selection des compteurs de vélos communs
 
 
 def get_common_counters_2019_to_2024(bike_counts_data_list):
@@ -29,7 +48,15 @@ def get_common_counters_2019_to_2024(bike_counts_data_list):
     return set.intersection(*df_id_compteurs)
 
 
-# Visualisation 1
+'''
+
+Les fonctions suivantes sont utilisés dans le pre-process de chaque visualisation.
+
+'''
+# Visualisation 1 : HeatMap
+# Determination de nombre quotidien de passage par vélo et formatage des dates
+
+
 def get_daily_bike_count(bike_counts_df):
     df = bike_counts_df.groupby('date')['nb_passages'].sum().reset_index()
     df['date'] = pd.to_datetime(df['date'])
@@ -38,28 +65,34 @@ def get_daily_bike_count(bike_counts_df):
     return df
 
 
-# Visualisation 2
+# Visualisation 2 : Polar Bar Chart
+ # Détermination de la saison en fonction du mois
 def get_season(month):
     if month in [12, 1, 2]:
         return 'Hiver'
     elif month in [3, 4, 5]:
-        return 'Printemps' 
+        return 'Printemps'
     elif month in [6, 7, 8]:
-        return 'Été' 
+        return 'Été'
     elif month in [9, 10, 11]:
-        return 'Automne'  
+        return 'Automne'
+ # Regroupement de la base de données en fonction de l'heure, saison et nombres de passages
+
 
 def get_hourly_bike_count(bike_counts_df):
     bike_counts_df['date'] = pd.to_datetime(bike_counts_df['date'])
-    bike_counts_df['season'] = bike_counts_df['date'].dt.month.apply(get_season)
+    bike_counts_df['season'] = bike_counts_df['date'].dt.month.apply(
+        get_season)
 
-    df = bike_counts_df.groupby(['heure', 'season'])['nb_passages'].sum().reset_index()
-    df['heure'] = pd.to_datetime(df['heure']).dt.strftime('%Hh') 
+    df = bike_counts_df.groupby(['heure', 'season'])[
+        'nb_passages'].sum().reset_index()
+    df['heure'] = pd.to_datetime(df['heure']).dt.strftime('%Hh')
     df = df.groupby(['heure', 'season'], as_index=False)['nb_passages'].sum()
     return df
 
 
-# Visualisation 3
+# Visualisation 3 : Carte des compteurs et pistes cyclables
+ # Calcul du nombre annuel de passages de vélos par compteur et intégration des données d'emplacement
 def get_yearly_counters_count(bike_counts_df):
     bike_counts_df['Année'] = pd.to_datetime(bike_counts_df['date']).dt.year
     yearly_count = bike_counts_df.groupby(['id_compteur', 'longitude', 'latitude', 'Année'])[
@@ -75,7 +108,8 @@ def get_yearly_counters_count(bike_counts_df):
     return pd.merge(yearly_count, counters_locations_df, on='id_compteur', how='inner')
 
 
-# Visualisation 4
+# Visualisation 4 : ScatterPlot
+ # Traitement et fusion des données de comptage avec les données météorologiques
 def get_daily_bike_count_with_weather(bike_counts_data_list, bike_counts_df):
     common_counters = get_common_counters_2019_to_2024(bike_counts_data_list)
     normalized_bike_counts = bike_counts_df[bike_counts_df['id_compteur'].isin(
